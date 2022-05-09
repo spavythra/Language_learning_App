20926e2e9fdb271fb9f2b9d6ce8bbdafc764cd6b
spavythra <spavythra@gmail.com>
2022-05-09 21:10:27 +0300

    backend files are under server folder

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
-const mysql = require('mysql')
+const mysql = require('mysql') 
+const cors = require('cors')
 require("dotenv").config()
+// const path = require('path');
+
 var port = process.env.PORT || 8000
 
-app.use(express.static("public"));
+
+app.use(cors());
 // console.log(process.env)
 // console.log(process.env.USER)
 // console.log(process.env)
@@ -23,19 +27,22 @@ let config = {
   var connection = mysql.createConnection(config);
   // connection.connect();
 
+  // console.log(process.env.NODE_ENV);
   
-
+  // if (process.env.NODE_ENV === "production") {
+    app.use(express.static("public"));
   app.get("/words", (req, res) => {
     connection.query("SELECT * from words", (error, results) => {
       if (error) {
         console.log(error);
         // connection.end();
       } else {
+        // res.sendFile(path.resolve(__dirname,  "public", "index.html"));
         res.send(results);
       }
     });
   });
-
+  // }
   
 
 app.listen(port , () => {
