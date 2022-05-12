9c24dfc41b1d6a47999e81a6fe9371d7bcd79ff3
spavythra <spavythra@gmail.com>
2022-05-12 20:44:04 +0300

    (App.js): words are displaying one by one

--- client/frontend/src/App.js
+++ client/frontend/src/App.js
@@ -3,8 +3,13 @@ import {useState, useEffect} from 'react'
 
 function App(){
     let [words, setWords] = useState([])
+    const [input, setInput] = useState('')
+	const [current, setCurrent] = useState(0)
+	
+	const [streak, setStreak] = useState(0)
+	const [maxStreak, setMaxStreak] = useState(0)
 
-    let [answer, setAnswer] = useState('')
+	const [error, setError] = useState(false)
 
   useEffect(()=>{
     async function getWords(){
@@ -12,38 +17,88 @@ function App(){
       let data = await hr.json()
       console.log(data)
       setWords(data);
+      setStreak(parseInt(localStorage.getItem('streak')) || 0)
+		setMaxStreak(parseInt(localStorage.getItem('maxStreak')) || 0)
     }
     getWords();
   }, []);
 
-  const animals = words.map((animal,i)=> <tr key={i}><td>{animal.Eng_word} </td><td> {animal.Fin_word}</td></tr>)
-  // function handleChange(event) {
-  //   // console.log(event.target.value);
-  //   setAnswer(event.target.value);
-  //   console.log(answer);
-  // }
+  const setNextWord = () => {
+		// const randomIndex = Math.floor(Math.random() * words.length)
+    // console.log(current)
 
-  // function checkAnswer(text){
-  //   if(answer == text){
-  //     console.log("correct")
-  //   } else {
-  //     console.log("wrong")
-  //   }
-  // }
+		// setCurrent(current+1)
+    // console.log(current)
+	}
 
-  // const animals = words.map((animal,i)=> <tr key={i}><td>{animal.Eng_word} </td><td><input onChange={handleChange}></input><button onClick={checkAnswer(animal.Fin_word)}>check</button></td></tr>)
+  // changing condition
+const handleChange = (event) => {
+setInput(event.target.value)
+}
 
- 
+//submit function
+const handleSubmit = (event) => {
+event.preventDefault()
+console.log(input.toLocaleLowerCase())
+console.log(words[current].Fin_word.toLocaleLowerCase())
+if(input.toLocaleLowerCase() === words[current].Fin_word.toLocaleLowerCase()){
+  setStreak(streak + 1)
+  setMaxStreak(streak + 1 > maxStreak ? streak + 1 : maxStreak)
+  setError(false)
+
+  localStorage.setItem('streak', streak + 1)
+  localStorage.setItem('maxStreak', streak + 1 > maxStreak ? streak + 1 : maxStreak)
+} else {
+  const h = words[current].Eng_word
+  const r = words[current].Fin_word
+  setError(`Wrong! The correct answer for ${h} is ${r}`)
+  setStreak(0)
+  localStorage.setItem('streak', 0)
+}
+
+setInput('')
+setCurrent(current+1)
+}
+
+  // const animals = words.map((animal,i)=><tr key={i}><td>{animal.Eng_word} </td><td><form onSubmit={handleSubmit}><input type="text" placeholder="Enter text" value={input} onChange={handleChange}/></form></td></tr>
+  // )
+
+
+
   return(
   <div>
     <div className='header'>Learning Finnish</div>
-    <div className='words'><table><tbody><tr>
+    <p>{streak} / {maxStreak}</p>
+    <div>
+				{words.map((word,i) => { if(i===current)
+        return(<p key={i}>{word.Eng_word}</p>)}
+        ) }
+          
+          
+			</div>
+        {/* <div className='words'><table><tbody><tr>
           <th>English Word</th>
           <th>Finnish word</th>
           
-        </tr>{animals}</tbody></table></div>
+        </tr>{animals}</tbody></table></div> */}
+
+      <div>
+				<form onSubmit={handleSubmit}>
+					<input
+						type="text"
+						onChange={handleChange}
+						value={input}
+						/>
+				</form>
+			</div>
+			{error && 
+				<div>
+					<p>{ error }</p>
+				</div>
+			}
+		</div>
     
-  </div>)
+  )
   
 }
 
