e90f258c89cacde039ae3765cb3e06d40c82b4bc
spavythra <spavythra@gmail.com>
2022-05-17 14:37:42 +0300

    (UpdateWord.js): update functionality added

--- client/frontend/src/UpdateWord.js
+++ client/frontend/src/UpdateWord.js
@@ -1,9 +1,91 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function UpdateWord() {
-  return (
-    <div>UpdateWord</div>
-  )
+    const [wordList, setWordList] = useState([]);
+
+    const [newWord, setNewWord] = useState('')
+
+    // const getWords = () => {
+    //     Axios.get("/words").then((response) => {
+    //         setWordList(response.data);
+    //     });
+    //   };
+
+    // getWords();
+
+    useEffect(()=>{
+        async function getWords(){
+          let hr = await fetch("/words")
+          let data = await hr.json()
+          setWordList(data);
+        }
+        getWords();
+      }, []);
+
+    const updateFinWord = (id) => {
+        Axios.put("/admin/update", { Fin_word: newWord, id: id }).then(
+      (response) => {
+        setWordList(
+            wordList.map((val) => {
+            return val.id == id
+              ? {
+                  id: val.id,
+                  Eng_word: val.Eng_word,
+                  Fin_word: newWord,
+                }
+              : val;
+          })
+        );
+      }
+    );
+    }
+    console.log(wordList)
+
+
+
+    
+        return (
+        <div>
+            <div>
+            { (() => {
+                return(<div>
+                    {wordList.map((val,i) => {
+                        return(<div key={i}>
+                            <div >
+                <h3 >English word: {val.Eng_word}</h3>
+                <h3 >Finnish word: {val.Fin_word}</h3>
+           
+                <input
+                  type="text"
+                  placeholder="update finnish word..."
+                  onChange={(event) => {
+                    setNewWord(event.target.value);
+                  }}
+                />
+                <button
+                  onClick={() => {
+                    updateFinWord(val.id);
+                  }}
+                > Update
+                </button>
+                </div>
+                        </div>)
+                    
+                 })
+        }
+                </div>)
+           
+    })()}
+    </div>
+             
+        </div>
+          )
+   
+
+
+    
 }
 
 export default UpdateWord
