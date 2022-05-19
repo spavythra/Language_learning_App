4eb089dc42f9296d1c9ce70b83f90975006bdf19
spavythra <spavythra@gmail.com>
2022-05-19 22:09:58 +0300

    (update): TotalScore style updated

--- client/frontend/src/TotalScore.js
+++ client/frontend/src/TotalScore.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { useNavigate } from 'react-router-dom';
 import { handleScoreChange } from "../src/redux/actions.js";
 import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 function TotalScore() {
     const disptach = useDispatch();
@@ -17,10 +18,13 @@ function TotalScore() {
   };
    
   return (
-    <div><h2> Final Score {score} </h2>
+    //   <div>Final</div>
+    <Box mt={30}>
+    <Typography variant="h3" fontWeight="bold" mb={3}> Final Score {score} </Typography>
     <Button onClick={handleBackToSettings} variant="outlined">
         back to settings!
-      </Button></div>
+      </Button>
+      </Box>
   )
 }
 
