8bd5b4257ef2a25b5015ac7da1649c8173e6676d
spavythra <spavythra@gmail.com>
2022-05-20 20:10:16 +0300

    (update): Addword, updated the buttons and title

--- client/frontend/src/AddWord.js
+++ client/frontend/src/AddWord.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from "react";
 import Axios from "axios";
 import { Button, Typography } from "@mui/material";
+import Home from "./Home.js"
 
 function AddWord() {
   const [Eng_word, setEng_word] = useState('')
@@ -27,27 +28,30 @@ function AddWord() {
   return (
     <div className='addword'>
       <div className="information">
-        <h3>Enter the words which you want to add</h3>
+        <h3 style={{marginBottom:'40px'}}>Add the new words here!!!</h3>
       <label>English word</label>
-      <input type="text" placeholder='Type your english word here..' onChange={(event) => {
+      <input type="text" style={{marginBottom:'40px'}} placeholder='Type a english word here..' onChange={(event) => {
             setEng_word(event.target.value);
           }}/>
       <label>Finnish word</label>
-      <input type="text" placeholder='Type your finnish word here..' onChange={(event) => {
+      <input type="text" style={{marginBottom:'40px'}} placeholder='Type a finnish word here..' onChange={(event) => {
             setFin_word(event.target.value);
           }}/>
+      <div style={{display: "flex", flexDirection:'row',width:'50%', justifyContent:'space-between'}}>
+            <Button
+              onClick={addWord}
+              variant="contained" style={{marginRight:'10px', display: "flex", flexDirection:'row', fontSize:'1.5rem', width:'50%',borderRadius:'25px', fontWeight:'bold'}} >
+              <span className="glyphicon btn-del-glyphicon glyphicon-plus img-fluid text-white"></span>
+              Add
+              </Button>
 
-      <Button
-        onClick={addWord}
-        variant="contained" style={{margin: '10 auto', display: "flex", flexDirection:'row', fontSize:'1.5rem', width:'30%'}} >
-        <span className="glyphicon btn-del-glyphicon glyphicon-trash img-fluid text-white"></span>
-        Add
-        </Button>
-
-      <a className="btn icon-btn btn-success" style={{marginTop:"20px", width:"20%"}} href="/">
-      <span className="glyphicon btn-glyphicon glyphicon-step-backward img-circle text-success"></span>
-      Go Home
-      </a>
+              <Button
+              onClick={()=> <Home/>}
+              variant="contained" style={{margin: '10 auto', display: "flex", flexDirection:'row', fontSize:'1.5rem', width:'50%', borderRadius:'25px', fontWeight:'bold'}} >
+              <span className="glyphicon btn-del-glyphicon glyphicon-home img-fluid text-white"></span>
+              Home
+              </Button>
+      </div>
       </div>
       
       
