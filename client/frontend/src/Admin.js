4141ee186d946f1c7ea4409eeae2338181d6e770
spavythra <spavythra@gmail.com>
2022-05-20 19:16:40 +0300

    (update): admin.js, cannot get error due to link tag

--- client/frontend/src/Admin.js
+++ client/frontend/src/Admin.js
@@ -11,19 +11,22 @@ function Admin() {
       Add  
       </a> */}
       
-      <Link className="btn icon-btn btn-success" to='/admin/add' ><span className="glyphicon btn-glyphicon glyphicon-plus img-circle text-success"></span>Add</Link>
-      <a className="btn icon-btn btn-info" href="/admin/update">
+      <Link className="btn icon-btn btn-success" to='/admin/add' >
+        <span className="glyphicon btn-glyphicon glyphicon-plus img-circle text-success"></span>
+        Add
+        </Link>
+      <Link className="btn icon-btn btn-info" to='/admin/update ' >
       <span className="glyphicon btn-glyphicon glyphicon-edit img-circle text-info"></span>
       Update
-      </a>
-      <a className="btn icon-btn btn-danger" href="/admin/delete">
+      </Link>
+      <Link className="btn icon-btn btn-danger" to='/admin/delete'>
       <span className="glyphicon btn-glyphicon glyphicon-trash img-circle text-danger"></span>
       Delete
-      </a>
-      <a className="btn icon-btn btn-warning" href="/">
+      </Link>
+      <Link className="btn icon-btn btn-warning" to="/">
       <span className="glyphicon btn-glyphicon glyphicon-step-backward img-circle text-warning"></span>
       Go Back
-      </a>
+      </Link>
       
     </nav>
    
